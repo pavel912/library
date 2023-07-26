2b596633476e540b44cceec3ed6447501d0af99f
pavel912 <gta4thebest@mail.ru>
2023-07-26 18:06:49 +0300

    setup spring and liquidbase

--- src/main/java/pavel/library/AppApplication.java
+++ src/main/java/pavel/library/AppApplication.java
@@ -1,7 +1,11 @@
 package pavel.library;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class AppApplication {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        SpringApplication.run(AppApplication.class, args);
     }
 }
--- src/test/java/pavel/library/AppTest.java
+++ src/test/java/pavel/library/AppTest.java
@@ -2,7 +2,9 @@ package pavel.library;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class AppTest {
 
     @Test
